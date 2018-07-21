23305bf53b33adafd28a8ab355354e45d1b02f60
kegranados1@gmail.com <kegranados1@gmail.com>
2018-07-21 01:24:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,13 +7,17 @@ function add(a,b) {
 function subtract(a,b) {
   return a - b 
 }
-
 function multiply(a,b) {
   return a * b
 }
-
 function divide(a,b) {
   return a / b
+}
+function inc(n) {
+  return n++
+}
+function dec(n) {
+  return n--
 }
 
 
